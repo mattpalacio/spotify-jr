7f7a15282f48dec4d88e9767c114b7debe1fdb4e
Matthew Palacio <mpalacio@catalyte.io>
2023-03-09 12:38:02 -0800

    refactors login endpoint

--- spotify-jr/spotify_jr/config.py
+++ spotify-jr/spotify_jr/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     spotify_client_secret: str
     spotify_auth_url: str
     spotify_api_url: str
-    redirect_url: str
+    redirect_uri: str
 
     class Config:
-        env_file = "../.env"
+        env_file = ".env"
--- spotify-jr/spotify_jr/main.py
+++ spotify-jr/spotify_jr/main.py
@@ -2,25 +2,26 @@ import base64
 import json
 from functools import lru_cache
 from requests import post, get
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Response, Request
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from . import config
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @lru_cache()
 def get_settings():
     return config.Settings()
-
-
-def get_code(settings: config.Settings):
-    scope = "user-library-read"
-
-    url = settings.spotify_auth_url + "/authorize"
-    query = f"?client_id={settings.spotify_client_id}&response_type=code&redirect_uri={settings.redirect_uri}&scope={scope}"
-    result = get(url + query)
-    print(result)
 
 
 def get_token(settings: config.Settings):
@@ -57,7 +58,7 @@ def get_token_using_code(code: str, settings: config.Settings):
     }
     result = post(url, headers=headers, data=data)
     json_result = json.loads(result.content)
-    token = json_result["access_token"]
+    token = json_result
     return token
 
 
@@ -82,25 +83,21 @@ def search_for_artist(token, artist_name, settings: config.Settings):
 
 
 @app.get("/login")
-async def home(settings: config.Settings = Depends(get_settings)):
-    get_code(settings=settings)
-    return None
+async def home(response: Response, settings: config.Settings = Depends(get_settings)):
+    scope = "user-library-read"
+    url = settings.spotify_auth_url + "/authorize"
+    query = f"?client_id={settings.spotify_client_id}&response_type=code&redirect_uri={settings.redirect_uri}&scope={scope}"
+    response = RedirectResponse(url=url + query)
+    return response
 
 
 @app.get("/callback")
 async def callback(
     code: str | None,
+    request: Request,
     settings: config.Settings = Depends(get_settings),
 ):
+    code = request.query_params["code"]
     token = get_token_using_code(code=code, settings=settings)
     print(token)
     return token
-
-
-@app.get("/artists/{artist_name}")
-async def get_artist(
-    artist_name: str, settings: config.Settings = Depends(get_settings)
-):
-    token = get_token(settings)
-    artist = search_for_artist(token=token, artist_name=artist_name, settings=settings)
-    return {"result": artist}
