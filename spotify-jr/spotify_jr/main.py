41ce14003e063128a3ad3cc5447d3b36a71f3da6
Matt Palacio <mtpalacio91@gmail.com>
2023-03-14 22:29:35 -0700

    adds player endpoints (#3)
    Co-authored-by: Matthew Palacio <mpalacio@catalyte.io>

--- spotify-jr/spotify_jr/main.py
+++ spotify-jr/spotify_jr/main.py
@@ -6,7 +6,7 @@ from urllib.parse import urlencode
 from fastapi import Depends, FastAPI, HTTPException, Query, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
-from requests import get, post
+from requests import get, post, put
 
 from . import config, schemas
 
@@ -30,7 +30,7 @@ def get_settings():
 async def authorize(
     response: Response, settings: config.Settings = Depends(get_settings)
 ):
-    scope = "user-library-read"
+    scope = "streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state"
     base_url = settings.spotify_auth_url + "/authorize"
     query_string = urlencode(
         {
@@ -124,6 +124,124 @@ async def search(
     if api_response.status_code == 200:
         data = json.loads(api_response.content)
         return data
+    else:
+        error = json.loads(api_response.content)["error"]
+        raise HTTPException(status_code=error["status"], detail=error["message"])
+
+
+@app.put("/player", status_code=204, tags=["player"])
+async def transfer_playback(
+    request: Request,
+    playback: schemas.PlaybackTransferIn,
+    settings: config.Settings = Depends(get_settings),
+):
+    bearer_token = request.headers["authorization"]
+    base_url = settings.spotify_api_url + "/me/player"
+    headers = {
+        "Authorization": bearer_token,
+        "Content-Type": "application/json",
+    }
+
+    json_data = {"device_ids": playback.device_ids, "play": playback.play}
+
+    api_response = put(url=base_url, headers=headers, json=json_data)
+
+    if api_response.status_code == 204:
+        return None
+    else:
+        error = json.loads(api_response.content)["error"]
+        raise HTTPException(status_code=error["status"], detail=error["message"])
+
+
+@app.get("/player/devices", tags=["player"])
+async def get_devices(
+    request: Request, settings: config.Settings = Depends(get_settings)
+):
+    bearer_token = request.headers["authorization"]
+    base_url = settings.spotify_api_url + "/me/player/devices"
+    headers = {
+        "Authorization": bearer_token,
+        "Content-Type": "application/json",
+    }
+
+    api_response = get(url=base_url, headers=headers)
+
+    if api_response.status_code == 200:
+        data = json.loads(api_response.content)
+        return data
+    else:
+        error = json.loads(api_response.content)["error"]
+        raise HTTPException(status_code=error["status"], detail=error["message"])
+
+
+@app.get("/player/currently-playing", tags=["player"])
+async def get_currently_playing(
+    request: Request, settings: config.Settings = Depends(get_settings)
+):
+    bearer_token = request.headers["authorization"]
+    base_url = settings.spotify_api_url + "/me/player/currently-playing"
+    query_string = urlencode({"additional_types": "track,episode", "market": "US"})
+    full_url = base_url + "?" + query_string
+    headers = {
+        "Authorization": bearer_token,
+        "Content-Type": "application/json",
+    }
+
+    api_response = get(url=full_url, headers=headers)
+
+    if api_response.status_code == 200:
+        data = json.loads(api_response.content)
+        return data
+    else:
+        error = json.loads(api_response.content)["error"]
+        raise HTTPException(status_code=error["status"], detail=error["message"])
+
+
+@app.put("/player/play", status_code=204, tags=["player"])
+async def start_playback(
+    request: Request,
+    device_id: str,
+    playback: schemas.PlaybackStartIn,
+    settings: config.Settings = Depends(get_settings),
+):
+    bearer_token = request.headers["authorization"]
+    base_url = settings.spotify_api_url + "/me/player/play"
+    query_string = urlencode({"device_id": device_id})
+    full_url = base_url + "?" + query_string
+    headers = {
+        "Authorization": bearer_token,
+        "Content-Type": "application/json",
+    }
+    json_data = {"uris": playback.uris, "position_ms": playback.position_ms}
+
+    api_response = put(url=full_url, headers=headers, json=json_data)
+
+    if api_response.status_code == 204:
+        return None
+    else:
+        error = json.loads(api_response.content)["error"]
+        raise HTTPException(status_code=error["status"], detail=error["message"])
+
+
+@app.put("/player/pause", status_code=204, tags=["player"])
+async def pause_playback(
+    request: Request,
+    device_id: str,
+    settings: config.Settings = Depends(get_settings),
+):
+    bearer_token = request.headers["authorization"]
+    base_url = settings.spotify_api_url + "/me/player/pause"
+    query_string = urlencode({"device_id": device_id})
+    full_url = base_url + "?" + query_string
+    headers = {
+        "Authorization": bearer_token,
+        "Content-Type": "application/json",
+    }
+
+    api_response = put(url=full_url, headers=headers)
+
+    if api_response.status_code == 204:
+        return None
     else:
         error = json.loads(api_response.content)["error"]
         raise HTTPException(status_code=error["status"], detail=error["message"])
