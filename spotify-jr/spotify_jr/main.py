7acc9e74eb1beccc0ec1a9e004d07d1cb2a782a6
Matthew Palacio <mpalacio@catalyte.io>
2023-04-05 12:20:09 -0700

    specifies origins

--- spotify-jr/spotify_jr/main.py
+++ spotify-jr/spotify_jr/main.py
@@ -5,9 +5,11 @@ from .routers import auth, player, search
 
 app = FastAPI()
 
+origins = ["http://localhost", "http://localhost:4200"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
